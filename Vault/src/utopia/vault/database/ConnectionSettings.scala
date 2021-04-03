b5077d79a817fa10a54069d1a469b395de99aac3
Mikko Hilpinen <mikko_hilpinen@hotmail.com>
2021-04-03 14:00:42 +0300

    Vault: Added option for charset collation edit in ConnectionSettings Aviation: Testing, debugging and fixing airports.dat file processing

--- Vault/src/utopia/vault/database/ConnectionSettings.scala
+++ Vault/src/utopia/vault/database/ConnectionSettings.scala
@@ -12,18 +12,31 @@ package utopia.vault.database
  * The mariaDB driver is used by default if nothing else is provided
  * @param debugPrintsEnabled Whether connection debug printing should be enabled
  * @param maximumAmountOfRowsCached The maximum number of rows requested on queries that map or operate on multiple rows
- * @param charsetName Name of the charset used when reading from DB. Please refer to MySql character set names in
+ * @param charsetName Name of the charset used when reading from DB (E.g. "UTF-8").
+ *                    Please refer to MySql character set names in
  *                    https://dev.mysql.com/doc/connector-j/5.1/en/connector-j-reference-charsets.html
+ * @param charsetCollationName Character set collation option. E.g. "utf8_general_ci"
  * @author Mikko Hilpinen
  * @since 16.4.2017
  */
 case class ConnectionSettings(connectionTarget: String = "jdbc:mysql://localhost:3306/", user: String = "root",
 							  password: String = "", defaultDBName: Option[String] = None,
 							  driver: Option[String] = None, debugPrintsEnabled: Boolean = false,
-							  maximumAmountOfRowsCached: Int = 10000, charsetName: String = "")
+							  maximumAmountOfRowsCached: Int = 10000, charsetName: String = "",
+							  charsetCollationName: String = "")
 {
 	/**
-	 * @return String inserted to driver manager connection in order to specify charset
+	 * @return String inserted to driver manager connection in order to specify charset and collation
 	 */
-	def charsetString = if (charsetName.isEmpty) "" else s"?useUnicode=true&characterEncoding=$charsetName"
+	def charsetString =
+	{
+		if (charsetName.isEmpty && charsetCollationName.isEmpty)
+			""
+		else
+		{
+			val charsetPart = if (charsetName.isEmpty) "" else s"&characterEncoding=$charsetName"
+			val collationPart = if (charsetCollationName.isEmpty) "" else s"&connectionCollation=$charsetCollationName"
+			s"?useUnicode=true$charsetPart$collationPart"
+		}
+	}
 }
