4805ca3a1c6a27082842222494e04fa36bbf43be
Mikko Hilpinen <mikko_hilpinen@hotmail.com>
2022-12-21 23:25:02 +0200

    Bugfix

--- Reach/src/utopia/reach/component/template/ReachComponentWrapper.scala
+++ Reach/src/utopia/reach/component/template/ReachComponentWrapper.scala
@@ -1,6 +1,6 @@
 package utopia.reach.component.template
 
-import utopia.paradigm.shape.shape2d.Bounds
+import utopia.paradigm.shape.shape2d.{Bounds, Vector2D}
 import utopia.genesis.util.Drawer
 import utopia.reflection.component.drawing.template.DrawLevel
 import utopia.reflection.component.template.layout.stack.StackableWrapper2
@@ -19,20 +19,25 @@ trait ReachComponentWrapper extends ReachComponentLike with StackableWrapper2
 	
 	// IMPLEMENTED	----------------------------
 	
+	override def parentHierarchy = wrapped.parentHierarchy
+	
 	override def transparent = wrapped.transparent
 	
-	override def children = wrapped.children
-	
 	override def positionPointer = wrapped.positionPointer
-	
 	override def sizePointer = wrapped.sizePointer
-	
 	override def boundsPointer = wrapped.boundsPointer
-	
-	override def parentHierarchy = wrapped.parentHierarchy
 	
 	override def paintContent(drawer: Drawer, drawLevel: DrawLevel, clipZone: Option[Bounds]) =
 		wrapped.paintContent(drawer, drawLevel, clipZone)
 	
+	override def bounds = wrapped.bounds
 	override def bounds_=(b: Bounds) = wrapped.bounds = b
+	
+	override def children = wrapped.children
+	
+	override def toImage = wrapped.toImage
+	
+	override def paintMovement(movement: => Vector2D) = wrapped.paintMovement(movement)
+	override def paintWith(drawer: Drawer, clipZone: Option[Bounds]) = wrapped.paintWith(drawer, clipZone)
+	override def regionToImage(region: Bounds) = wrapped.regionToImage(region)
 }
