544e2fa07b30e842709aefdfe7c5e2ab71386992
Mikko Hilpinen <mikko_hilpinen@hotmail.com>
2023-05-22 22:43:38 -0500

    Working on new Scribe logger implementation (server-side)

--- Scribe/Scribe-Api/src/utopia/scribe/api/controller/logging/Scribe.scala
+++ Scribe/Scribe-Api/src/utopia/scribe/api/controller/logging/Scribe.scala
@@ -1,12 +1,21 @@
 package utopia.scribe.api.controller.logging
 
-import utopia.flow.util.logging.Logger
+import utopia.flow.util.logging.{Logger, SysErrLogger}
+import utopia.scribe.api.controller.logging.Scribe.loggingQueue
 import utopia.scribe.core.model.enumeration.Severity
 import utopia.scribe.core.model.enumeration.Severity.Unrecoverable
+import utopia.vault.util.DatabaseActionQueue
 
 object Scribe
 {
+	// ATTRIBUTES   ---------------------
 	
+	private lazy val loggingQueue = {
+		import utopia.scribe.api.util.ScribeContext._
+		// TODO: Use a more sophisticated backup logger
+		implicit val backupLogger: Logger = SysErrLogger
+		DatabaseActionQueue()
+	}
 }
 
 /**
@@ -17,9 +26,19 @@ object Scribe
   * @param defaultSeverity The default level of [[Severity]] recorded by this logger (default = Unrecoverable)
   */
 // TODO: Add logToConsole and logToFile -options (to ScribeContext) once the basic features have been implemented
+// TODO: Once basic features have been added, consider adding an email integration or other trigger-actions
 case class Scribe(context: String, defaultSeverity: Severity = Unrecoverable) extends Logger
 {
 	// IMPLEMENTED  -------------------------
 	
-	def apply(error: Option[Throwable], message: String) = ???
+	// TODO: Add parameter for additional details (String)
+	def apply(error: Option[Throwable], message: String) = loggingQueue.push { implicit c =>
+		// TODO: Implement by
+		//  1) Extracting stack trace elements,
+		//  2) Storing them to the database (check for duplicates),
+		//  3) Storing the error to the database (check for duplicates),
+		//  4) Pulling for existing issue + variant (make combined model),
+		//  5) Inserting issues and variants where necessary,
+		//  6) Recording a new occurrence
+	}
 }
