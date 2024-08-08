9629c7757d6c430cff6bfb0171da3f5bd13a9808
Mikkomario <mikko@victoryfoundations.com>
2024-08-07 22:31:47 -0500

    Debugging inheritance features

--- Vault/src/utopia/vault/nosql/storable/StorableFactory.scala
+++ Vault/src/utopia/vault/nosql/storable/StorableFactory.scala
@@ -5,10 +5,13 @@ import utopia.vault.model.immutable.{DbPropertyDeclaration, Storable}
 /**
   * A partial implementation for storable model factories,
   * which also provide utility access to database property declarations.
-  * @author Mikko Hilpinen
+  * @tparam DbModel Type of constructed Storable instances
+ * @tparam Complete Type of complete / stored instances
+ * @tparam Data Type of the data instances used
+ * @author Mikko Hilpinen
   * @since 16/03/2024, v1.19
   */
-trait StorableFactory[+DbModel <: Storable, +Complete, Data] extends DataInserter[DbModel, Complete, Data]
+trait StorableFactory[+DbModel <: Storable, +Complete, -Data] extends DataInserter[DbModel, Complete, Data]
 {
 	// OTHER    -------------------------
 	
