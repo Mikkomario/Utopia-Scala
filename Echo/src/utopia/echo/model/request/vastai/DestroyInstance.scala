091e5728e8fb17660227bee837808aac035bc26e
Mikkomario <mikko@victoryfoundations.com>
2026-02-25 10:19:07 -0500

    Added requests for destroying and listing instances

---
+++ Echo/src/utopia/echo/model/request/vastai/DestroyInstance.scala
@@ -0,0 +1,48 @@
+package utopia.echo.model.request.vastai
+
+import utopia.access.model.enumeration.Method
+import utopia.access.model.enumeration.Method.Delete
+import utopia.annex.controller.ApiClient
+import utopia.annex.model.request.ApiRequest
+import utopia.annex.model.response.RequestResult
+import utopia.disciple.model.error.RequestFailedException
+import utopia.disciple.model.request.Body
+import utopia.flow.generic.model.immutable.{Model, Value}
+import utopia.flow.view.immutable.View
+import utopia.flow.view.immutable.eventful.AlwaysFalse
+
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
+/**
+ * Ends the rent by deleting the instance and all associated data. Irreversible.
+ * @author Mikko Hilpinen
+ * @since 25.02.2026, v1.4.1
+ */
+case class DestroyInstance(instanceId: Int, deprecationView: View[Boolean] = AlwaysFalse) extends ApiRequest[String]
+{
+	// ATTRIBUTES   -------------------------
+	
+	override val method: Method = Delete
+	override val path: String = s"instances/$instanceId"
+	override val pathParams: Model = Model.empty
+	override val body: Either[Value, Body] = Left(Value.empty)
+	
+	
+	// IMPLEMENTED  -------------------------
+	
+	override def deprecated: Boolean = deprecationView.value
+	
+	override def send(prepared: ApiClient.PreparedRequest): Future[RequestResult[String]] =
+		prepared.parseValue { body =>
+			body.model match {
+				case Some(body) =>
+					if (body("success").booleanOr(true))
+						Success(body("msg").getString)
+					else
+						Failure(new RequestFailedException(
+							body("msg").stringOr("Instance-deletion failed (no message included)")))
+				case None => Success("")
+			}
+		}
+}
---
+++ Echo/src/utopia/echo/model/request/vastai/ShowInstances.scala
@@ -0,0 +1,36 @@
+package utopia.echo.model.request.vastai
+
+import utopia.annex.controller.ApiClient
+import utopia.annex.model.request.GetRequest
+import utopia.annex.model.response.RequestResult
+import utopia.echo.model.vastai.instance.VastAiInstance
+import utopia.flow.collection.CollectionExtensions._
+import utopia.flow.generic.model.immutable.Model
+
+import scala.concurrent.Future
+
+/**
+ * A request used for showing all reserved instances for the user
+ * @author Mikko Hilpinen
+ * @since 25.02.2026, v1.4.1
+ */
+object ShowInstances extends GetRequest[Seq[VastAiInstance]]
+{
+	// ATTRIBUTES   ------------------------
+	
+	override val path: String = "instances"
+	override val pathParams: Model = Model.empty
+	override val deprecated: Boolean = false
+	
+	
+	// IMPLEMENTED  -----------------------
+	
+	override def send(prepared: ApiClient.PreparedRequest): Future[RequestResult[Seq[VastAiInstance]]] =
+		prepared.parseValue { body =>
+			body.getModelOrVector match {
+				case Left(body) =>
+					body.tryGet("instances") { _.tryVectorWith { _.tryModel.flatMap(VastAiInstance.apply) } }
+				case Right(values) => values.tryMapAll { _.tryModel.flatMap(VastAiInstance.apply) }
+			}
+		}
+}
